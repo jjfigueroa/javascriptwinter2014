25f5a2bbbdf7f66ca436c97048ae44535af3e9ce
josmely <josmely@DrOz-PC.home>
2014-03-19 17:07:17 -0400

    Merge origin/master
    Conflicts:
    	nbproject/project.properties
    	public_html/Wk3/Demo/week3.html
    	public_html/Wk3/Lab/index.html
    	public_html/Wk3/Lab/scripts.js
    	public_html/Wk3/Lab/style.css

--- public_html/week7/lab/script.js
+++ public_html/week7/lab/script.js
@@ -8,7 +8,28 @@
        
                     $("a").mouseover(function() {
     $(this).css('color', 'blue');
+   
 }).mouseout(function() {
     $(this).css('color', 'black');
 });
-
+ 
+                 $("#navb1").click(function() {
+                 $("#nav2,#nav3,#nav4").hide();
+                 $("#nav1").fadeIn("slow");
+                 
+             });
+            
+                   $("#navb2").click(function() {
+                 $("#nav1,#nav3,#nav4").hide();
+                  $("#nav2").fadeIn("slow");
+                   });
+             
+                   $("#navb3").click(function() {
+                 $("#nav1,#nav2,#nav4").hide();
+                  $("#nav3").fadeIn("slow");
+             });
+                   $("#navb4").click(function() {
+                 $("#nav1,#nav2,#nav3").hide();
+                  $("#nav4").fadeIn("slow");
+             });
+     
